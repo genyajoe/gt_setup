9fc558912bbbebf5dddf6f593d7b96ba1a614a3e
Joe Ishigaki <pausch.g@gmail.com>
2015-04-11 21:43:29 +0900

    modify shell script to fix scipy error

--- b3_setup.sh
+++ b3_setup.sh
@@ -8,12 +8,14 @@ sudo apt-get install -y build-essential python-dev
 sudo apt-get update
 
 #install pip
-sudo easy_install python-pip
+sudo easy_install pip
 
-sudo apt-get install python-numpy
-sudo apt-get install python-scipy
-sudo apt-get install python-matplotlib
-sudo apt-get install python-networkx
+sudo apt-get install -y python-numpy
+
+#if you have "scipy has no installation cadidate" error, check the "software sources" tab in software center (from Edit). When "universe" is not marked, modify it.
+sudo apt-get install -y python-scipy
+sudo apt-get install -y python-matplotlib
+sudo apt-get install -y python-networkx
 
 sudo pip install pip-tools
 
