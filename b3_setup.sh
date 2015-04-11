dc6c978710e95cc4650db3949ff907a7cd505492
Joe Ishigaki <pausch.g@gmail.com>
2015-04-11 21:26:25 +0900

    modify shell script to fix pip issues on ubuntu

--- b3_setup.sh
+++ b3_setup.sh
@@ -1,19 +1,19 @@
 sudo apt-get update
 
 #install easy_install
-sudo apt-get install python-setuptools
+sudo apt-get install -y python-setuptools
 
-sudo apt-get install build-essential python-dev
+sudo apt-get install -y build-essential python-dev
 
 sudo apt-get update
 
 #install pip
-sudo easy_install pip
+sudo easy_install python-pip
 
-sudo pip install numpy
-sudo pip install scipy
-sudo pip install matplotlib
-sudo pip install networkx
+sudo apt-get install python-numpy
+sudo apt-get install python-scipy
+sudo apt-get install python-matplotlib
+sudo apt-get install python-networkx
 
 sudo pip install pip-tools
 
